d9dbd7f36e77d950e76395af6c9774cdd837da89
lugglory <lugglory@gmail.com>
2026-04-01 01:36:37 +0900

    Update claudo.bat

--- scripts/claudo.bat
+++ scripts/claudo.bat
@@ -1,2 +1,3 @@
 @echo off
-dockerdoor claude --model opusplan --permission-mode bypassPermissions %*
+if "%~1"=="" (set MODEL=opusplan) else (set MODEL=%1)
+dockerdoor claude --permission-mode bypassPermissions --model %MODEL%
